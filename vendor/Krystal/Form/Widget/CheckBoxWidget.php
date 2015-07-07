ffea10d1da9c1984ec0fe636b782c3d1074458ab
David Yang <daworld.ny@gmail.com>
2015-07-07 21:52:05 +0500

    Added CheckBoxWidget

---
+++ vendor/Krystal/Form/Widget/CheckBoxWidget.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * This file is part of the Krystal Framework
+ * 
+ * Copyright (c) 2015 David Yang <daworld.ny@gmail.com>
+ * 
+ * For the full copyright and license information, please view
+ * the license file that was distributed with this source code.
+ */
+
+namespace Krystal\Form\Widget;
+
+use Krystal\Form\Element\Checkbox;
+
+class CheckBoxWidget
+{
+	/**
+	 * Renders checkbox
+	 * 
+	 * @param array $attrs Element attributes
+	 * @param mixed $checked Positive or negative value that reflects checking state
+	 * @param boolean $serialize Whether it should be serializeable or not
+	 * @return string
+	 */
+	public static function render(array $attrs, $checked, $serialize = true)
+	{
+		$element = new Checkbox($serialize, $checked);
+		return $element->render($attrs);
+	}
+}
