bb79a98caeffec1319a20e5bc3fe532a87e23385
David Yang <daworld.ny@gmail.com>
2015-07-17 07:19:40 +0500

    Fixed StartsWith constraint

---
+++ vendor/Krystal/Validate/Input/Constraint/StartsWith.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * This file is part of the Krystal Framework
+ * 
+ * Copyright (c) 2015 David Yang <daworld.ny@gmail.com>
+ * 
+ * For the full copyright and license information, please view
+ * the license file that was distributed with this source code.
+ */
+
+namespace Krystal\Validate\Constraint;
+
+final class StartsWith extends AbstractConstraint
+{
+	/**
+	 * A character that needs to be found
+	 * 
+	 * @var string
+	 */
+	private $needle;
+
+	/**
+	 * {@inheritDoc}
+	 */
+	protected $message = 'Given string does not start with a required character';
+
+	/**
+	 * State initialization
+	 * 
+	 * @param string $needle
+	 * @return void
+	 */
+	public function __construct($needle)
+	{
+		$this->needle = $needle;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public function isValid($haystack)
+	{
+		if (strncmp($haystack, $this->needle, strlen($this->needle)) === 0) {
+			return true;
+		} else {
+
+			$this->violate($this->message);
+			return false;
+		}
+	}
+}
