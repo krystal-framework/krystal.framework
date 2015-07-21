83a1057b32a69334897eaff787773b9c8910f4e8
David Yang <daworld.ny@gmail.com>
2015-07-22 03:14:35 +0500

    Added UrlPathGenerator

---
+++ vendor/Krystal/Http/FileTransfer/UrlPathGenerator.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * This file is part of the Krystal Framework
+ * 
+ * Copyright (c) 2015 David Yang <daworld.ny@gmail.com>
+ * 
+ * For the full copyright and license information, please view
+ * the license file that was distributed with this source code.
+ */
+
+namespace Krystal\Http\FileTransfer;
+
+final class UrlPathGenerator implements UrlPathGeneratorInterface
+{
+	/**
+	 * Base URL path
+	 * 
+	 * @var string
+	 */
+	private $baseUrl;
+
+	/**
+	 * State initialization
+	 * 
+	 * @param string $baseUrl
+	 * @return void
+	 */
+	public function __construct($baseUrl)
+	{
+		$this->baseUrl = $baseUrl;
+	}
+
+	/**
+	 * Returns full URL path to a file
+	 * 
+	 * @param string $id Nested directory's id
+	 * @param string $filename Required filename
+	 * @return string
+	 */
+	public function getPath($id, $filename)
+	{
+		return sprintf('%s/%s/%s', $this->baseUrl, $id, $filename);
+	}
+}
---
+++ vendor/Krystal/Http/FileTransfer/UrlPathGeneratorInterface.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * This file is part of the Krystal Framework
+ * 
+ * Copyright (c) 2015 David Yang <daworld.ny@gmail.com>
+ * 
+ * For the full copyright and license information, please view
+ * the license file that was distributed with this source code.
+ */
+
+namespace Krystal\Http\FileTransfer;
+
+interface UrlPathGeneratorInterface
+{
+	/**
+	 * Returns full URL path to a file
+	 * 
+	 * @param string $id Nested directory's id
+	 * @param string $filename Required filename
+	 * @return string
+	 */
+	public function getPath($id, $filename);
+}
