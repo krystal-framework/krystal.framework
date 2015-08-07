bd790713fa04d136f7bcbbf13a2384eec4eb2ddc
David Yang <daworld.ny@gmail.com>
2015-08-07 13:41:31 +0500

    Added missing contracts

--- vendor/Krystal/Application/Module/ModuleManagerInterface.php
+++ vendor/Krystal/Application/Module/ModuleManagerInterface.php
@@ -13,5 +13,63 @@ namespace Krystal\Application\Module;
 
 interface ModuleManagerInterface
 {
-	//@TODO
+	/**
+	 * Initializes the module manager
+	 * 
+	 * @throws \RuntimeException If no modules found
+	 * @return void
+	 */
+	public function initialize();
+
+	/**
+	 * Returns module instance by its name
+	 * 
+	 * @param object $name
+	 * @return \Krystal\Application\Module\AbstractModule
+	 */
+	public function getModule($name);
+
+	/**
+	 * Checks whether module is loaded
+	 * 
+	 * @param string $name Module name to be checked
+	 * @return boolean
+	 */
+	public function isLoaded($name);
+
+	/**
+	 * Returns an array of loaded module names
+	 * 
+	 * @return array
+	 */
+	public function getLoadedModuleNames();
+
+	/**
+	 * Returns a collection of loaded module instances
+	 * 
+	 * @return array
+	 */
+	public function getLoadedModules();
+
+	/**
+	 * Append routes to the global stack
+	 * 
+	 * @param array $routes
+	 * @return void
+	 */
+	public function appendRoutes(array $routes);
+
+	/**
+	 * Returns all routes
+	 * 
+	 * @return array
+	 */
+	public function getRoutes();
+
+	/**
+	 * Returns all merged translations
+	 * 
+	 * @return array
+	 */
+	public function getTranslations();
 }
