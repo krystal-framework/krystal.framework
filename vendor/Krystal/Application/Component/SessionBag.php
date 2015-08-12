125544f8d6c00112d2fdbb03ef3049ba031fff0b
David Yang <daworld.ny@gmail.com>
2015-08-13 03:56:46 +0500

    Finished initialization logic

--- vendor/Krystal/Application/Component/SessionBag.php
+++ vendor/Krystal/Application/Component/SessionBag.php
@@ -14,6 +14,9 @@ namespace Krystal\Application\Component;
 use Krystal\Session\SessionBag as Component;
 use Krystal\Application\InputInterface;
 use Krystal\InstanceManager\DependencyInjectionContainerInterface;
+use Krystal\Session\Adapter;
+use RunitmeException;
+use LogicException;
 
 final class SessionBag implements ComponentInterface
 {
@@ -22,12 +25,56 @@ final class SessionBag implements ComponentInterface
 	 */
 	public function getInstance(DependencyInjectionContainerInterface $container, array $config, InputInterface $input)
 	{
+		// Defaults
+		$handler = null;
+		$cookieParams = array();
+
+		// Start configuring if possible
+		if (isset($config['components']['session'])) {
+			// Just a short-cut
+			$config = $config['components']['session'];
+
+			// Alter default $cookieParams if needed
+			if (isset($config['cookie_params']) && is_array($config['cookie_params'])) {
+				$cookieParams = $config['cookie_params'];
+			}
+
+			// Check if there's a custom session handler
+			if (isset($config['handler'])) {
+				switch ($config['handler']) {
+					case 'sql':
+
+						// Make sure the database component is available, before processing the rest
+						if (!$container->exists('db')) {
+							throw new LogicException('Can not use SQL session handler without configured database connection');
+						}
+
+						// Grab available database connection
+						$db = $container->get('db');
+
+						if (isset($config['options']['connection']) && isset($config['options']['table'])) {
+							// Grab connection's name
+							$connection = $db[$config['options']['connection']];
+							
+							// Now alter default handler
+							$handler = new Adapter\Sql($connection->getPdo(), $config['options']['table']);
+
+						} else {
+							throw new LogicException('No connection or table name defined for session service');
+						}
+
+					break;
+
+					default:
+						throw new RunitmeException(sprintf('Unsupported session handler supplied "%s"', $config['handler']));
+				}
+			}
+		}
+
 		$cookieBag = $container->get('request')->getCookieBag();
+		$sessionBag = new Component($cookieBag, $handler);
 
-		$sessionBag = new Component($cookieBag);
-		//@TODO Inject cookie options
-		$sessionBag->start();
-
+		$sessionBag->start($cookieParams);
 		return $sessionBag;
 	}
 
