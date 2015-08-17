8993c61c46487f234e6a758a74d0da5960e679df
David Yang <daworld.ny@gmail.com>
2015-08-17 06:46:55 +0500

    Moved shared bootstrapping logic to standalone file

---
+++ config/bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+
+// Make paths relative to the root folder
+chdir(dirname(__DIR__));
+
+// Include application factory
+require(dirname(__DIR__).'/vendor/Krystal/Application/AppFactory.php');
+
+// Return prepared application's instance
+return Krystal\Application\AppFactory::build(require(__DIR__.'/application.php'));
--- index.php
+++ index.php
@@ -1,7 +1,4 @@
 <?php
 
-// Make paths relative to the root folder
-chdir(__DIR__);
-
-require('/vendor/Krystal/Application/AppFactory.php');
-Krystal\Application\AppFactory::build(require('config/application.php'))->run();
+$app = require(__DIR__.'/config/bootstrap.php');
+$app->run();
