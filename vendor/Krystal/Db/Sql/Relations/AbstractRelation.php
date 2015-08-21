40aa3c20bbd55ad594282b6883f2943d0317ee56
David Yang <daworld.ny@gmail.com>
2015-08-21 15:47:57 +0500

    Added a structure for relations

---
+++ vendor/Krystal/Db/Sql/Relations/AbstractRelation.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * This file is part of the Krystal Framework
+ * 
+ * Copyright (c) 2015 David Yang <daworld.ny@gmail.com>
+ * 
+ * For the full copyright and license information, please view
+ * the license file that was distributed with this source code.
+ */
+
+namespace Krystal\Db\Sql\Relations;
+
+use Krystal\Db\Sql\DbInterface;
+
+abstract class AbstractRelation
+{
+	/**
+	 * Database service
+	 * 
+	 * @var \Krystal\Db\Sql\DbInterface
+	 */
+	protected $db;
+
+	/**
+	 * State initialization
+	 * 
+	 * @param \Krystal\Db\Sql\DbInterface $db Database service
+	 * @return void
+	 */
+	public function __construct(DbInterface $db)
+	{
+		$this->db = $db;
+	}
+}
---
+++ vendor/Krystal/Db/Sql/Relations/OneToOne.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * This file is part of the Krystal Framework
+ * 
+ * Copyright (c) 2015 David Yang <daworld.ny@gmail.com>
+ * 
+ * For the full copyright and license information, please view
+ * the license file that was distributed with this source code.
+ */
+
+namespace Krystal\Db\Sql\Relations;
+
+final class OneToOne extends AbstractRelation
+{
+	
+}
