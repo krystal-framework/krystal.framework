4d091b3ba70f616665a006a4abdf5a036dfd4d61
David Yang <daworld.ny@gmail.com>
2015-09-03 18:49:29 +0500

    Improved internals and added 'when' handler

--- vendor/Krystal/Application/Component/AppConfig.php
+++ vendor/Krystal/Application/Component/AppConfig.php
@@ -14,6 +14,8 @@ namespace Krystal\Application\Component;
 use Krystal\Application\AppConfig as Component;
 use Krystal\InstanceManager\DependencyInjectionContainerInterface;
 use Krystal\Application\InputInterface;
+use Krystal\Http\RequestInterface;
+use RuntimeException;
 
 final class AppConfig implements ComponentInterface
 {
@@ -22,32 +24,12 @@ final class AppConfig implements ComponentInterface
 	 */
 	public function getInstance(DependencyInjectionContainerInterface $container, array $config, InputInterface $input)
 	{
-		// Special configuration for language section
-		if (isset($config['components']['translator'])) {
-			// Just a reference as a short-hand
-			$translator =& $config['components']['translator'];
-
-			// Visitor function has a higher priority
-			if (isset($translator['visitor']) && is_callable($translator['visitor'])) {
-				// Invoke a visitor
-				$language = $translator['visitor']($container, $config);
-				// If we have a default language
-			} elseif (isset($translator['default'])) {
-				$language = $translator['default'];
-			} else {
-				// We don't have a visitor and we don't have a default language here
-			}
-
-		} else {
-			// Here we don't have a config at all
-			$language = 'en';
-		}
-
 		if (!isset($config['components']['view']['theme'])) {
-			throw new \Exception('You should provide a theme');
+			throw new RuntimeException('You should provide a theme');
 		}
-		
+
 		$request = $container->get('request');
+		$language = $this->getLanguage($config, $request);
 
 		$appConfig = new Component();
 		$appConfig->setRootDir(isset($config['root_dir']) ? $config['root_dir'] : $request->getRootDir())
@@ -63,6 +45,54 @@ final class AppConfig implements ComponentInterface
 				  ->setModulesDir(isset($config['module_dir']) ? $config['module_dir'] : $appConfig->getRootDir() . '/module');
 
 		return $appConfig;
+	}
+
+	/**
+	 * Returns language value to be used for request
+	 * 
+	 * @param array $translator Translator's configuration
+	 * @param \Krystal\Http\RequestInterface $request
+	 * @return string
+	 */
+	private function getLanguage(array $config, RequestInterface $request)
+	{
+		// Special configuration for language section
+		if (isset($config['components']['translator'])) {
+
+			// Just a reference as a short-hand
+			$translator =& $config['components']['translator'];
+
+			// Visitor function has a higher priority
+			if (isset($translator['visitor']) && is_callable($translator['visitor'])) {
+				// Invoke a visitor
+				$language = $translator['visitor']($container, $config);
+				// If we have a default language
+			} elseif (isset($translator['default'])) {
+				$language = $translator['default'];
+			} else {
+				// We don't have a visitor and we don't have a default language here
+			}
+
+			// Must have lower priority
+			if (isset($translator['when'], $translator['when']['uri'], $translator['when']['language'])) {
+				if (!is_array($translator['when']['uri'])) {
+					$translator['when']['uri'] = (array) $translator['when']['uri'];
+				}
+
+				foreach ($translator['when']['uri'] as $uri) {
+					if (strpos($request->getURI(), $uri) !== false) {
+						$language = $translator['when']['language'];
+					}
+				}
+			}
+
+		} else {
+
+			// Use default language
+			$language = 'en';
+		}
+
+		return $language;
 	}
 
 	/**
