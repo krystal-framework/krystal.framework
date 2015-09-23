8a1fb4b67a2070a910fcb518501136945cb3904c
David Yang <daworld.ny@gmail.com>
2015-09-23 19:43:31 +0500

    Updated the contract

--- vendor/Krystal/Http/Response/HttpCacheInterface.php
+++ vendor/Krystal/Http/Response/HttpCacheInterface.php
@@ -1,8 +1,24 @@
 <?php
 
+/**
+ * This file is part of the Krystal Framework
+ * 
+ * Copyright (c) No Global State Lab
+ * 
+ * For the full copyright and license information, please view
+ * the license file that was distributed with this source code.
+ */
+
 namespace Krystal\Http\Response;
 
 interface HttpCacheInterface
 {
-	
+	/**
+	 * Starts to capture
+	 * 
+	 * @param integer $timestamp Last modified timestamp
+	 * @param integer $ttl Time to live in seconds
+	 * @return void
+	 */
+	public function configure($timestamp, $ttl);
 }
