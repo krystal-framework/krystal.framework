43de38ee4528426dcc771ad3b07e827431bb5e48
David Yang <daworld.ny@gmail.com>
2015-09-23 19:50:06 +0500

    Minor updates

--- config/app.php
+++ config/app.php
@@ -1,6 +1,14 @@
 <?php
 
-/* Most options are already set by default, therefore they are hidden */
+/**
+ * This file is part of the Krystal Framework
+ * 
+ * Copyright (c) No Global State Lab
+ * 
+ * For the full copyright and license information, please view
+ * the license file that was distributed with this source code.
+ */
+
 return array(
 
 	'production' => false,
