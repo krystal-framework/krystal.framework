b2521aa02a0379ed4b24e51f7bff84413aaf47a0
David Yang <daworld.ny@gmail.com>
2015-09-23 19:47:40 +0500

    Added missing copyrights

--- vendor/Krystal/Paginate/Tests/PaginatorTest.php
+++ vendor/Krystal/Paginate/Tests/PaginatorTest.php
@@ -1,5 +1,14 @@
 <?php
 
+/**
+ * This file is part of the Krystal Framework
+ * 
+ * Copyright (c) No Global State Lab
+ * 
+ * For the full copyright and license information, please view
+ * the license file that was distributed with this source code.
+ */
+
 namespace Krystal\Paginate\Tests;
 
 use Krystal\Paginate\Paginator;
