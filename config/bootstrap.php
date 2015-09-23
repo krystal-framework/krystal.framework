eff1a4146bdda01b91f1f19a45e7b9d90493d76d
David Yang <daworld.ny@gmail.com>
2015-09-23 19:34:49 +0500

    Added missing copyrights

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,5 +1,14 @@
 <?php
 
+/**
+ * This file is part of the Krystal Framework
+ * 
+ * Copyright (c) No Global State Lab
+ * 
+ * For the full copyright and license information, please view
+ * the license file that was distributed with this source code.
+ */
+
 // Make paths relative to the root folder
 chdir(dirname(__DIR__));
 
