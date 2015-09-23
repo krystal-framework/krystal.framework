8dfffcd9188e9abd7523f55065e03331e8376626
David Yang <daworld.ny@gmail.com>
2015-09-23 19:31:25 +0500

    Updated copyrights

--- vendor/Krystal/Validate/File/Constraint/AbstractConstraint.php
+++ vendor/Krystal/Validate/File/Constraint/AbstractConstraint.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of the Krystal Framework
  * 
- * Copyright (c) 2015 David Yang <daworld.ny@gmail.com>
+ * Copyright (c) No Global State Lab
  * 
  * For the full copyright and license information, please view
  * the license file that was distributed with this source code.
