3a321f9d24dfdbd3daea42de12493fa58761e1c2
David Yang <daworld.ny@gmail.com>
2015-10-05 04:53:27 +0500

    Made getUploadsUrl() work

--- vendor/Krystal/Application/Component/AppConfig.php
+++ vendor/Krystal/Application/Component/AppConfig.php
@@ -33,13 +33,14 @@ final class AppConfig implements ComponentInterface
 
 		$appConfig = new Component();
 		$appConfig->setRootDir(isset($config['root_dir']) ? $config['root_dir'] : $request->getRootDir())
+				  ->setRootUrl(isset($config['root_url']) ? $config['root_url'] : $request->getBaseUrl())
 				  ->setDataDir(isset($config['data_dir']) ? $config['data_dir'] : $appConfig->getRootDir() . '/data')
 				  ->setUploadsDir(isset($config['uploads_dir']) ? $config['uploads_dir'] : $appConfig->getDataDir() . '/uploads')
+				  ->setUploadsUrl(isset($config['uploads_url']) ? $config['uploads_dir'] : $appConfig->getRootUrl() . '/data/uploads')
 				  ->setLanguage($language)
 				  ->setTheme($config['components']['view']['theme'])
 				  ->setThemesDir(isset($config['themes_dir']) ? $config['themes_dir'] : $appConfig->getRootDir(). '/themes')
 				  ->setThemeDir($appConfig->getThemesDir().'/'.$appConfig->getTheme())
-				  ->setRootUrl(isset($config['root_url']) ? $config['root_url'] : $request->getBaseUrl())
 				  ->setTempDir(isset($config['temp_dir']) ? $config['temp_dir'] : $appConfig->getDataDir().'/tmp')
 				  ->setCacheDir(isset($config['cache_dir']) ? $config['cache_dir'] : $appConfig->getDataDir().'/cache')
 				  ->setModulesDir(isset($config['module_dir']) ? $config['module_dir'] : $appConfig->getRootDir() . '/module');
