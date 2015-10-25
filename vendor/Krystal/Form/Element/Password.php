856d684ba7c965ce9ed99e03c28b590868b1623d
David Yang <daworld.ny@gmail.com>
2015-10-25 20:32:44 +0500

    Added password element generator

---
+++ vendor/Krystal/Form/Element/Password.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * This file is part of the Krystal Framework
+ * 
+ * Copyright (c) No Global State Lab
+ * 
+ * For the full copyright and license information, please view
+ * the license file that was distributed with this source code.
+ */
+
+namespace Krystal\Form\Element;
+
+use Krystal\Form\NodeElement;
+
+final class Password implements FormElementInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function render(array $attrs)
+    {
+        $attrs['type'] = 'password';
+        $node = new NodeElement();
+
+        return $node->openTag('input')
+                    ->addAttributes($attrs)
+                    ->finalize(true)
+                    ->render();
+	}
+}
