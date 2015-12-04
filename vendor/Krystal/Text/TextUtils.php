e7c94571f5e91b1e75319465edb610b21a335760
David Yang <daworld.ny@gmail.com>
2015-12-04 08:02:48 +0500

    Added empty TextUtils class

---
+++ vendor/Krystal/Text/TextUtils.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * This file is part of the Krystal Framework
+ * 
+ * Copyright (c) No Global State Lab
+ * 
+ * For the full copyright and license information, please view
+ * the license file that was distributed with this source code.
+ */
+
+namespace Krystal\Text;
+
+class TextUtils
+{
+    
+}
