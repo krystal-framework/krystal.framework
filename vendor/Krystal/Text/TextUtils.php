bfa725d33f755ecec8810b5693f0052800cb460f
David Yang <daworld.ny@gmail.com>
2015-12-04 08:06:56 +0500

    Added multiExplode()

--- vendor/Krystal/Text/TextUtils.php
+++ vendor/Krystal/Text/TextUtils.php
@@ -13,5 +13,31 @@ namespace Krystal\Text;
 
 class TextUtils
 {
-    
+    /**
+     * Explodes a string into array supporting several delimiters
+     * 
+     * @param string $string
+     * @param array $delimiters A collection of delimiters
+     * @param boolean $keepDelimiters Whether to keep delimiters on exploding
+     * @return array
+     */
+    public static function multiExplode($string, array $delimiters, $keepDelimiters = true)
+    {
+        if ($keepDelimiters === true) {
+            // Ensure special characters are escaped
+            foreach ($delimiters as &$delimiter) {
+                $delimiter = preg_quote($delimiter);
+            }
+
+            // RegEx fragment with delimiters
+            $fragment = implode('|', $delimiters);
+            $regEx = sprintf('@(?<=%s)@', $fragment);
+            return preg_split($regEx, $string, -1, PREG_SPLIT_DELIM_CAPTURE);
+
+        } else {
+
+            $string = str_replace($delimiters, $delimiters[0], $string);
+            return explode($delimiters[0], $string);
+        }
+    }
 }
