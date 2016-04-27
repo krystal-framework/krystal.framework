d1e07e9e53cd7493389729f3d0059eb6f560450a
David Yang <daworld.ny@gmail.com>
2016-04-27 23:06:13 +0500

    Prepared for unit-testing

---
+++ src/Krystal/ParamBag/Tests/ParamBagTest.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * This file is part of the Krystal Framework
+ * 
+ * Copyright (c) No Global State Lab
+ * 
+ * For the full copyright and license information, please view
+ * the license file that was distributed with this source code.
+ */
+
+namespace Krystal\ParamBag\ParamBag\Tests;
+
+use Krystal\ParamBag\ParamBag;
+
+class ParamBagTest extends \PHPUnit_Framework_TestCase
+{
+    
+}
