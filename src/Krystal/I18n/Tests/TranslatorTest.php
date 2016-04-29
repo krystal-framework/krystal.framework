e96d88c55fed43cc203c7287c76b474862b0b042
David Yang <daworld.ny@gmail.com>
2016-04-30 00:21:01 +0500

    Added unit test for Translator

---
+++ src/Krystal/I18n/Tests/TranslatorTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Krystal\I18n\Tests;
+
+use Krystal\I18n\Translator;
+
+class TranslatorTest extends \PHPUnit_Framework_TestCase
+{
+    private $translator;
+    private $defaults = array(
+        'To do something' => 'Zu machen etwas'
+    );
+
+    public function setUp()
+    {
+        $this->translator = new Translator($this->defaults);
+    }
+
+    public function testCanTranslate()
+    {
+        $this->assertEquals($this->translator->translate('To do something'), 'Zu machen etwas');
+    }
+
+    public function testTargetSentenceExists()
+    {
+        $this->assertTrue($this->translator->exists('To do something'));
+    }
+
+    public function testCanBeExtended()
+    {
+        $this->translator->extend(array(
+            'I must do it' => 'Ich muss machen das'
+        ));
+
+        $this->assertEquals($this->translator->translate('I must do it'), 'Ich muss machen das');
+    }
+
+    public function testReturnsTargetOnEmptyDictionary()
+    {
+        $this->translator->reset();
+        $this->assertEquals($this->translator->translate('Doing a test'), 'Doing a test');
+    }
+}
