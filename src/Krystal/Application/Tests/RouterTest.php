756e53a64e5c19529d9ed7539216f91006dc45e7
David Yang <daworld.ny@gmail.com>
2016-05-14 23:54:20 +0500

    Added route for static segment

--- src/Krystal/Application/Tests/RouterTest.php
+++ src/Krystal/Application/Tests/RouterTest.php
@@ -2,7 +2,18 @@
 
 namespace Krystal\Application\Tests;
 
+use Krystal\Application\Route\Router;
+use Krystal\Application\Route\RouteMatch;
+
 class RouterTest extends \PHPUnit_Framework_TestCase
 {
-    
+    public function testCanMatchStaticSegment()
+    {
+        $router = new Router();
+        $result = $router->match('/about-us', array(
+            '/home',
+            '/about-us'
+        ));
+
+    }
 }
