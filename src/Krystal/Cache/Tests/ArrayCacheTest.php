326f3fa2fdab89eb2a97e2422229cf46d269776a
David Yang <daworld.ny@gmail.com>
2016-06-12 23:51:13 +0500

    Initialization with sample data

--- src/Krystal/Cache/Tests/ArrayCacheTest.php
+++ src/Krystal/Cache/Tests/ArrayCacheTest.php
@@ -6,5 +6,27 @@ use Krystal\Cache\FileEngine\ArrayCache;
 
 class ArrayCacheTest extends \PHPUnit_Framework_TestCase
 {
-    
+    private $arrayCache;
+
+    public function setUp()
+    {
+        $this->arrayCache = new ArrayCache();
+        $this->arrayCache->setData(array(
+            'name' => array(
+                ArrayCache::CACHE_PARAM_VALUE => 'Jack',
+                ArrayCache::CACHE_PARAM_TTL => 10,
+                ArrayCache::CACHE_PARAM_CREATED => time(),
+            ),
+            'age' => array(
+                ArrayCache::CACHE_PARAM_VALUE => 30,
+                ArrayCache::CACHE_PARAM_TTL => 10,
+                ArrayCache::CACHE_PARAM_CREATED => time(),
+            ),
+            'location' => array(
+                ArrayCache::CACHE_PARAM_VALUE => 'Palo Alto',
+                ArrayCache::CACHE_PARAM_TTL => 10,
+                ArrayCache::CACHE_PARAM_CREATED => time(),
+            )
+        ));
+    }
 }
