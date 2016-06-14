9618def229e32e80f126d1782e29f411077a056b
David Yang <daworld.ny@gmail.com>
2016-06-14 22:54:39 +0500

    Added test case for ArraySignature

--- src/Krystal/Cache/Tests/ArraySignatureTest.php
+++ src/Krystal/Cache/Tests/ArraySignatureTest.php
@@ -6,5 +6,21 @@ use Krystal\Cache\FileEngine\ArraySignature;
 
 class ArraySignatureTest extends \PHPUnit_Framework_TestCase
 {
-    
+    public function testCanDetectChange()
+    {
+        $initial = array(
+            'name' => 'Dave',
+            'age' => 24
+        );
+
+        $updated = array(
+            'name' => 'Dave',
+            'age' => 25
+        );
+
+        $singature = new ArraySignature();
+        $singature->setData($initial);
+
+        $this->assertTrue($singature->hasChanged($updated));
+    }
 }
