5fcf0c34bb5d4d395a7bb7dce3de2d51ae3428cb
David Yang <daworld.ny@gmail.com>
2016-07-24 22:09:23 +0500

    Minor updates in status HTTP response status codes

--- src/Krystal/Http/Response/StatusGenerator.php
+++ src/Krystal/Http/Response/StatusGenerator.php
@@ -29,8 +29,11 @@ final class StatusGenerator implements StatusGeneratorInterface
      * @var array
      */
     private $statuses = array(
+        // Basic informational
         100 => 'Continue',
         101 => 'Switching Protocols',
+        102 => 'Processing',
+        // Success
         200 => 'OK',
         201 => 'Created',
         202 => 'Accepted',
@@ -38,13 +41,18 @@ final class StatusGenerator implements StatusGeneratorInterface
         204 => 'No Content',
         205 => 'Reset Content',
         206 => 'Partial Content',
+        207 => 'Multi-status',
+        208 => 'Already Reported',
+        // Redirection
         300 => 'Multiple Choices',
         301 => 'Moved Permanently',
         302 => 'Found',
         303 => 'See Other',
         304 => 'Not Modified',
         305 => 'Use Proxy',
+        306 => 'Switch Proxy',
         307 => 'Temporary Redirect',
+        // Client errors
         400 => 'Bad Request',
         401 => 'Unauthorized',
         402 => 'Payment Required',
@@ -53,23 +61,36 @@ final class StatusGenerator implements StatusGeneratorInterface
         405 => 'Method Not Allowed',
         406 => 'Not Acceptable',
         407 => 'Proxy Authentication Required',
-        408 => 'Request Timeout',
+        408 => 'Request Time-out',
         409 => 'Conflict',
         410 => 'Gone',
         411 => 'Length Required',
         412 => 'Precondition Failed',
         413 => 'Request Entity Too Large',
-        414 => 'Request-URI Too Long',
+        414 => 'Request-URI Too Large',
         415 => 'Unsupported Media Type',
-        416 => 'Requested Range Not Satisfiable',
+        416 => 'Requested range not satisfiable',
         417 => 'Expectation Failed',
-        418 => 'I am a teapot',
+        418 => 'I\'m a teapot',
+        422 => 'Unprocessable Entity',
+        423 => 'Locked',
+        424 => 'Failed Dependency',
+        425 => 'Unordered Collection',
+        426 => 'Upgrade Required',
+        428 => 'Precondition Required',
+        429 => 'Too Many Requests',
+        431 => 'Request Header Fields Too Large',
+        // Server errors
         500 => 'Internal Server Error',
         501 => 'Not Implemented',
         502 => 'Bad Gateway',
         503 => 'Service Unavailable',
-        504 => 'Gateway Timeout',
-        505 => 'HTTP Version Not Supported'
+        504 => 'Gateway Time-out',
+        505 => 'HTTP Version not supported',
+        506 => 'Variant Also Negotiates',
+        507 => 'Insufficient Storage',
+        508 => 'Loop Detected',
+        511 => 'Network Authentication Required'
     );
 
     /**
