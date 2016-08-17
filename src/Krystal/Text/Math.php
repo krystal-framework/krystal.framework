08b67f184d0c266182729342fbba5cc4013b1193
David Yang <daworld.ny@gmail.com>
2016-08-17 08:01:48 +0500

    Added percentage()

--- src/Krystal/Text/Math.php
+++ src/Krystal/Text/Math.php
@@ -13,4 +13,22 @@ namespace Krystal\Text;
 
 class Math
 {
+    /**
+     * Counts a percentage
+     * 
+     * @param float|integer $total
+     * @param float|integer $actual
+     * @param integer 
+     * @return mixed
+     */
+    public static function percentage($total, $actual, $round = 1)
+    {
+        $value = 100 * $actual / $total;
+
+        if (is_integer($round)) {
+            $value = round($value, $round);
+        }
+
+        return $value;
+    }
 }
