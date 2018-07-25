124b495017b3592497a8a6ccbad10a55a9d024b6
dave-yang <daworld.ny@gmail.com>
2018-07-26 00:34:17 +0500

    Added SlideStyle (damaged file fix)

---
+++ src/Krystal/Paginate/Style/SlideStyle.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * This file is part of the Krystal Framework
+ * 
+ * Copyright (c) No Global State Lab
+ * 
+ * For the full copyright and license information, please view
+ * the license file that was distributed with this source code.
+ */
+
+namespace Krystal\Paginate\Style;
+
+final class SlideStyle implements StyleInterface
+{
+    /**
+     * Amount of pages
+     * 
+     * @var integer
+     */
+    private $step;
+
+    /**
+     * State initialization
+     * 
+     * @param integer $amount
+     * @return void
+     */
+    public function __construct($step)
+    {
+        $this->step = $step;
+    }
+
+    /**
+     * Filter page numbers via current style adapter
+     * 
+     * @param array $page Array of page numbers
+     * @param integer $current Current page number
+     * @return array
+     */
+    public function getPageNumbers(array $pages, $current)
+    {
+        $offset = $current * $this->step;
+        return array_slice($pages, $offset, $this->step);
+    }
+}
