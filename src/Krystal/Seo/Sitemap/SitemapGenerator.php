a78b3e1741052e3959627b6eedb41ef3727d0333
dave-yang <daworld.ny@gmail.com>
2020-04-15 15:44:09 +0500

    Drop img NS
    Not required in default implementation

--- src/Krystal/Seo/Sitemap/SitemapGenerator.php
+++ src/Krystal/Seo/Sitemap/SitemapGenerator.php
@@ -18,7 +18,6 @@ final class SitemapGenerator extends AbstractGenerator
      */
     protected $rootAttributes = array(
         'xmlns' => 'http://www.sitemaps.org/schemas/sitemap/0.9',
-        'xmlns:image' => 'http://www.google.com/schemas/sitemap-image/1.1',
         'xmlns:xsi' => 'http://www.w3.org/2001/XMLSchema-instance'
     );
 
