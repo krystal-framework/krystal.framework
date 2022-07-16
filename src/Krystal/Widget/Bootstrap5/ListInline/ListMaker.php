f36a7f54a0a98e677b34c9773e5b90b8b7d452bc
dave-yang <daworld.ny@gmail.com>
2022-07-17 00:03:32 +0500

    Added list inline widget

---
+++ src/Krystal/Widget/Bootstrap5/ListInline/ListMaker.php
@@ -0,0 +1,103 @@
+<?php
+
+/**
+ * This file is part of the Krystal Framework
+ * 
+ * Copyright (c) No Global State Lab
+ * 
+ * For the full copyright and license information, please view
+ * the license file that was distributed with this source code.
+ */
+
+namespace Krystal\Widget\Bootstrap5\ListInline;
+
+use Krystal\Form\NodeElement;
+
+final class ListMaker
+{
+    /**
+     * List data
+     * 
+     * @var array
+     */
+    private $items = [];
+
+    /**
+     * List group options
+     * 
+     * @var array
+     */
+    private $classes = [];
+
+    /**
+     * State initialization
+     * 
+     * @param array $items
+     * @param array $classes
+     * @return void
+     */
+    public function __construct(array $items = [], array $classes = [])
+    {
+        $this->items = $items;
+        $this->classes = $classes;
+    }
+
+    /**
+     * State initialization
+     * 
+     * @param array $items
+     * @return void
+     */
+    public function render()
+    {
+        $ulClass = 'list-inline';
+        $liClass = 'list-inline-item';
+        
+        if (isset($this->classes['ul'])) {
+            $ulClass .= ' ' . $this->classes['ul'];
+        }
+
+        if (isset($this->classes['li'])) {
+            $liClass .= ' ' . $this->classes['li'];
+        }
+
+        $ul = new NodeElement();
+        $ul->openTag('ul')
+           ->addAttribute('class', $ulClass)
+           ->finalize();
+
+        foreach ($this->items as $item) {
+            $li = new NodeElement('li');
+            $li->openTag('li')
+               ->addAttribute('class', $liClass);
+
+            // Set text
+            if (isset($item['text']) && !isset($item['link'])) {
+                $li->setText($item['text']);
+            }
+
+            // Set link
+            if (isset($item['link'], $item['text'])) {
+                $a = new NodeElement();
+                $a->openTag('a');
+
+                if (isset($this->classes['a'])) {
+                    $a->addAttribute('class', $this->classes['a']);
+                }
+
+                $a->addAttribute('href', $item['link'])
+                  ->setText($item['text'])
+                  ->closeTag();
+
+                $li->appendChild($a);
+            }
+
+            $li->closeTag();
+            $ul->appendChild($li);
+        }
+
+        $ul->closeTag();
+
+        return $ul->render();
+    }
+}
---
+++ src/Krystal/Widget/Bootstrap5/ListInline/README.md
@@ -0,0 +1,57 @@
+Floating form
+----
+
+This widget generates inline list with optional links.
+
+# Usage example
+
+    <?php
+    
+    use Krystal\Widget\Bootstrap5\ListInline\ListMaker;
+    
+    $items = [
+        [
+            'text' => 'Facebook'
+        ],
+    
+        [
+            'text' => 'Google'
+        ],
+    
+        [
+            'link' => 'https://microsoft.com',
+            'text' => 'Microsoft'
+        ]
+    ];
+    
+    $lm = new ListMaker($items);
+    echo $lm->render();
+
+This will generate the following output:
+
+    <ul class="list-inline">
+        <li class="list-inline-item">Facebook</li>
+        <li class="list-inline-item">Google</li>
+        <li class="list-inline-item"><a href="https://gmail.com">Gmail</a></li>
+    </ul>
+
+You might want to append extra CSS classes to list nodes. You can supply an array of extra classes for each element as a second argument on instantiation:
+
+    $classes = [
+        'ul' => 'w-50',
+        'li' => 'fw-bold',
+        'a' => 'text-decoration-none'
+    ];
+    
+    $lm = new ListMaker($items, $classes);
+    echo $lm->render();
+
+
+This will generate the following output:
+
+    <ul class="list-inline w-50">
+        <li class="list-inline-item fw-bold">Facebook</li>
+        <li class="list-inline-item fw-bold">Google</li>
+        <li class="list-inline-item fw-bold"><a class="text-decoration-none" href="https://gmail.com">Gmail</a></li>
+    </ul>
+    
