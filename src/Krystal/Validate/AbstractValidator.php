6c715d68a26059cf7bdd1c396d756f930e5e144c
dave-yang <daworld.ny@gmail.com>
2025-10-12 22:06:58 +0500

    Updated copyrights

--- src/Krystal/Validate/AbstractValidator.php
+++ src/Krystal/Validate/AbstractValidator.php
@@ -3,8 +3,6 @@
 /**
  * This file is part of the Krystal Framework
  * 
- * Copyright (c) No Global State Lab
- * 
  * For the full copyright and license information, please view
  * the license file that was distributed with this source code.
  */
