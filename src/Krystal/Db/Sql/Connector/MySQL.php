dbf1be366735f18cacd30f6358eec45270c31b42
dave-yang <daworld.ny@gmail.com>
2026-01-25 20:16:23 +0500

    Fixed collation

--- src/Krystal/Db/Sql/Connector/MySQL.php
+++ src/Krystal/Db/Sql/Connector/MySQL.php
@@ -24,14 +24,14 @@ final class MySQL implements ConnectorInterface
             $dsn .= ';dbname='.$config['dbname'];
         }
 
-        $dsn .= ';charset=utf8';
+        $dsn .= ';charset=utf8mb4';
 
         return array($dsn, $config['username'], $config['password'], array(
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_EMULATE_PREPARES => true,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
             // Disable STRICT mode
-            PDO::MYSQL_ATTR_INIT_COMMAND => 'SET sql_mode="IGNORE_SPACE,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION", NAMES utf8'
+            PDO::MYSQL_ATTR_INIT_COMMAND => 'SET sql_mode="IGNORE_SPACE,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION"'
         ));
     }
 }
